1cad00c283476a5ab73d8d9909b3c327f97c2305
yoyodes1000 <yoyodes1000@gmail.com>
2024-04-18 09:29:18 +0200

    voila

---
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-onomatopia.component.html',
+  styleUrl: './create-onomatopia.component.scss'
+})
+export class CreateOnomatopiaComponent {
+
+}
