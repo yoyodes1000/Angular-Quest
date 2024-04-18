09a02be48e521a5bbad3c4c3bb6d5a12f6c492d0
yoyodes1000 <yoyodes1000@gmail.com>
2024-04-18 09:59:12 +0200

    quest 8 OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CreateOnomatopiaComponent } from './create-onomatopia/create-onomatopia.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CreateOnomatopiaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MyFirstApp';
+  title= "Pimpoye";
+  onomatopiaList: string[] = ["ba", "be", "bi", "bou"];
+
+  onReceiveOnomatopia(event: string): void {
+    this.onomatopiaList.push(event);
+    console.log(this.onomatopiaList);
+  }
 }
--- src/app/create-onomatopia/create-onomatopia.component.ts
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-onomatopia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-onomatopia.component.html',
   styleUrl: './create-onomatopia.component.scss'
 })
 export class CreateOnomatopiaComponent {
 
+  newOnomatopia : string = "";
+
+  @Output()
+  sendOnomatopiaToParent : EventEmitter<string> = new EventEmitter();
+
+  createOnomatopia(): void {
+    this.sendOnomatopiaToParent.emit(this.newOnomatopia);
+  }
 }
